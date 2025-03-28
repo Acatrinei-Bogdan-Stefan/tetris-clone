b11bdf6a00cc2554c2186098261d616efde30ef6
Bogdan Acatrinei <bogdan.z9.acatrinei@gmail.com>
2025-03-28 15:11:55 +0200

    Version 0.07

--- src/game.cpp
+++ src/game.cpp
@@ -58,7 +58,7 @@ void Game::HandleInput()
 void Game::MoveBlockLeft()
 {
   currentBlock.Move(0, -1);
-  if(IsBlockOutside())
+  if(IsBlockOutside() || BlockFits() == false)
   {
     currentBlock.Move(0, 1);
   }
@@ -67,7 +67,7 @@ void Game::MoveBlockLeft()
 void Game::MoveBlockRight()
 {
   currentBlock.Move(0, 1);
-  if(IsBlockOutside())
+  if(IsBlockOutside() || BlockFits() == false)
   {
     currentBlock.Move(0, -1);
   }
@@ -76,7 +76,7 @@ void Game::MoveBlockRight()
 void Game::MoveBlockDown()
 {
   currentBlock.Move(1, 0);
-  if(IsBlockOutside())
+  if(IsBlockOutside() || BlockFits() == false)
   {
     currentBlock.Move(-1, 0);
     LockBlock();
@@ -99,7 +99,7 @@ bool Game::IsBlockOutside()
 void Game::RotateBlock()
 {
   currentBlock.Rotate();
-  if(IsBlockOutside())
+  if(IsBlockOutside() || BlockFits() == false)
   {
     currentBlock.UndoRotation();
   }
@@ -113,10 +113,19 @@ void Game::LockBlock()
     grid.grid[item.row][item.column] = currentBlock.id;
   }
   currentBlock = nextBlock;
-  nextBlock = GetRandomBlock();
+  nextBlock = GetRandomBlock();  
+  grid.ClearFullRows();
 }
 
 bool Game::BlockFits()
 {
-  return false;
+  std::vector<Position> tiles = currentBlock.GetCellPosition();
+  for(Position item: tiles)
+  {
+    if(grid.IsCellEmpty(item.row, item.column) == false)
+    {
+      return false;
+    }
+  }
+  return true;
 }
